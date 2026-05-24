c0afb3bc137b9934e4155453e830b182cb5905a1
Akash06december <rbt23et015@gmail.com>
2026-05-24 19:13:32 +0530

    rendering events home page

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
+const events = require("./data/events")
 
 const port = 8080;
-app.set("view engine" , "views");
+app.set("view engine" , "ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -11,8 +12,13 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
     res.render("index.ejs")
+});
+
+app.get("/events" , (req,res)=>{
+    res.render("events", {events});
 })
+
 
 app.listen(port,()=>{
     console.log("Server Started")
-})
+});
